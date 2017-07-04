e3973c6f5731093389bdbf76f94620ab746885da
Brandon Johnson-Murphy <brandon.johnsonmurphy@gmail.com>
2017-07-04 06:56:13 -0300

    Add files via upload

---
+++ webDevBootcampUdemy/introJQuery/exercise.js
@@ -0,0 +1,11 @@
+// Make all divs have a purple background.
+$("div").css("background", "purple");
+
+// Select divs with highlight class and make them 200px wide.
+$(".highlight").css("width", "200px");
+
+// Select div with third id and give it an orange border.
+$("#third").css("border", "2px solid orange");
+
+// Bonus: Select first div and change its font to pink.
+$("div:first-of-type").css("color", "pink");
